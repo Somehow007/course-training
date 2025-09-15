263851d88c277ff542a2b8dee1b78ad6db936dfa
somehow_mac <sunhao04012006@163.com>
2025-09-15 14:07:11 +0800

    fix: 修复启动类mapper扫描路径错误问题

--- src/main/java/com/ujs/trainingprogram/tp/TpApplication.java
+++ src/main/java/com/ujs/trainingprogram/tp/TpApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.ujs.trainingprogram.tp.mapper")
+@MapperScan("com.ujs.trainingprogram.tp.dao.mapper")
 public class TpApplication {
 
 	public static void main(String[] args) {
