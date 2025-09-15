85f5f19cadf91998b92a7256df4df2f5c33b3b95
somehow_mac <sunhao04012006@163.com>
2025-09-15 14:06:49 +0800

    optimize: 优化mp自动添加源数据

--- src/main/java/com/ujs/trainingprogram/tp/config/DataBaseConfiguration.java
+++ src/main/java/com/ujs/trainingprogram/tp/config/DataBaseConfiguration.java
@@ -1,17 +1,24 @@
 package com.ujs.trainingprogram.tp.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Date;
+
+/**
+ * 数据库持久层配置类
+ */
 @Configuration
 public class DataBaseConfiguration {
 
     /**
-     * 分页插件
+     * Mybatis-Plus MySQL 分页插件
      */
     @Bean
     @ConditionalOnMissingBean
@@ -19,5 +26,32 @@ public class DataBaseConfiguration {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
+    }
+
+    /**
+     * 注册 Mybatis-Plus 源数据自动填充类
+     */
+    @Bean
+    public MyMetaObjectHandler myMetaObjectHandler() {
+        return new MyMetaObjectHandler();
+    }
+
+    /**
+     * Mybatis-Plus 源数据自动填充类
+     */
+    static class MyMetaObjectHandler implements MetaObjectHandler {
+
+
+        @Override
+        public void insertFill(MetaObject metaObject) {
+            strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+            strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+            strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+        }
+
+        @Override
+        public void updateFill(MetaObject metaObject) {
+            strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        }
     }
 }
